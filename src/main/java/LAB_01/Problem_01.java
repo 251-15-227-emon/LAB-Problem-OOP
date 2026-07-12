823818bc311fdd4cfc42542e1f60b9243bc6710e
251-15-227-emon <251-15-227@diu.edu.bd>
2026-07-12 19:58:04 +0600

    Updated LAB_01 package

--- src/main/java/LAB_01/Problem_01.java
+++ src/main/java/LAB_01/Problem_01.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package LAB_01;
 
-/**
- *
- * @author EMON
- */
+class Book {
+
+    String title;
+    String author;
+    double price;
+
+    Book() {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0.0;
+    }
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.price = 0.0;
+    }
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+        System.out.println();
+    }
+}
+
 public class Problem_01 {
-    
+
+    public static void main(String[] args) {
+
+        Book b1 = new Book();
+        Book b2 = new Book("Himu", "Humayun Ahmed");
+        Book b3 = new Book("Lilabotir Mrittu", "Humayun Ahmed", 450.50);
+
+        b1.display();
+        b2.display();
+        b3.display();
+    }
+
 }
