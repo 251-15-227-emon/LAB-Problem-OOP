47eb6c86a3788d87cf5c6b707a8cb658bc71f2b8
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-19 19:02:08 +0600

    Update EmployeeManagementSystem.java

--- src/main/java/Spring2025/EmployeeManagementSystem.java
+++ src/main/java/Spring2025/EmployeeManagementSystem.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Spring2025;
 
-/**
- *
- * @author EMON
- */
+class Employee {
+
+
+
+
+}
+
 public class EmployeeManagementSystem {
     
 }
