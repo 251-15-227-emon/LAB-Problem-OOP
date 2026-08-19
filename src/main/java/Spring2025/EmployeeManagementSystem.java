5fbb2566c3b775ed913e763990a8f0f9e52dcce1
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-19 19:06:32 +0600

    Update EmployeeManagementSystem.java

--- src/main/java/Spring2025/EmployeeManagementSystem.java
+++ src/main/java/Spring2025/EmployeeManagementSystem.java
@@ -2,11 +2,36 @@ package Spring2025;
 
 class Employee {
 
+    String name;
+    int id;
+    double salary;
 
+    Employee(String name, int id, double salary) {
 
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+
+    }
+
+    void calculateSalary() {
+
+        System.out.println("Slary :" + salary);
+
+    }
+
+    void details() {
+        System.out.println("Employee Name : " + name);
+        System.out.println("Employee ID   : " + id);
+
+    }
 
 }
 
+
+
+
+
 public class EmployeeManagementSystem {
-    
+
 }
