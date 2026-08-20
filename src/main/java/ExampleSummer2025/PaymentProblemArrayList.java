e355cd67b03c5984e33cca26a291dcf5e3064644
251-15-227-emon <251-15-227@diu.edu.bd>
2026-08-20 22:07:23 +0600

    new code

---
+++ src/main/java/ExampleSummer2025/PaymentProblemArrayList.java
@@ -0,0 +1,81 @@
+package ExampleSummer2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Payment {
+
+    double amount;
+
+    Payment(double amount) {
+
+        this.amount = amount;
+
+    }
+
+    abstract void processPayment();
+
+}
+
+class CreditCardPayment extends Payment {
+
+    String cardNumber, cardHolder;
+
+    CreditCardPayment(double amount, String cardNumber, String cardHolder) {
+
+        super(amount);
+        this.cardNumber = cardNumber;
+        this.cardHolder = cardHolder;
+    }
+
+    @Override
+    public void processPayment() {
+
+        System.out.println("Card Number :" + cardNumber);
+        System.out.println("Card Holder Name :" + cardHolder);
+        System.out.println("Payment Amount : " + amount);
+
+    }
+}
+
+class MobilePayment extends Payment {
+
+    String MobileNumber, Provider;
+
+    MobilePayment(double amount, String MobileNumber, String Provider) {
+
+        super(amount);
+        this.MobileNumber = MobileNumber;
+        this.Provider = Provider;
+    }
+
+    @Override
+    public void processPayment() {
+
+        System.out.println("Mobile Number :" + MobileNumber);
+        System.out.println("Provider :" + Provider);
+        System.out.println("Payment Amount : " + amount);
+
+    }
+}
+
+public class PaymentProblemArrayList {
+
+    public static void main(String[] args) {
+
+        List <Payment> Payments = new ArrayList<>();
+
+        Payments.add(new CreditCardPayment(5000, "0121715189", "Emon"));
+        Payments.add(new MobilePayment(4000, "0171715189", "Alim"));
+
+        for (Payment p : Payments) {
+
+            p.processPayment();
+
+            System.out.println();
+
+        }
+
+    }
+
+}
